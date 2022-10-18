370e73dae4abaed6b03fcd907c9110c1a7c9251b
Fabrice Didierjean <coredmp95@gmail.com>
2022-10-18 19:57:31 +0200

    Test Deployment

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 python manage.py wait_for_db
-python manage.py colllectstatic --noinput
+python manage.py collectstatic --noinput
 python manage.py migrate
 
-uswgi --socket :9000  --workers 4 --master --enable-thread --module app.wsgi
+uwsgi --socket :9000  --workers 4 --master --enable-thread --module app.wsgi
